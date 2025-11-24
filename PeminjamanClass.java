0b5e1e22145b1b499d8a3129dd12a571727119a4
tashaaagr <tasya12091996@gmail.com>
2025-11-24 09:02:21 +0700

    Add PeminjamanBuku class for book borrowing
    Signed-off-by: tashaaagr <tasya12091996@gmail.com>

--- PeminjamanClass.java
+++ PeminjamanClass.java
@@ -1,1 +1,58 @@
+import java.time.LocalDate;
+
+public class PeminjamanBuku {
+
+    private ItemPerpustakaan item;
+    private String namaPeminjam;
+    private LocalDate tanggalPinjam;
+    private LocalDate tanggalKembali;
+
+    public PeminjamanBuku(ItemPerpustakaan item, String namaPeminjam,
+                          LocalDate tanggalPinjam, LocalDate tanggalKembali) {
+        this.item = item;
+        this.namaPeminjam = namaPeminjam;
+        this.tanggalPinjam = tanggalPinjam;
+        this.tanggalKembali = tanggalKembali;
+    }
+
+    public ItemPerpustakaan getItem() {
+        return item;
+    }
+
+    public void setItem(ItemPerpustakaan item) {
+        this.item = item;
+    }
+
+    public String getNamaPeminjam() {
+        return namaPeminjam;
+    }
+
+    public void setNamaPeminjam(String namaPeminjam) {
+        this.namaPeminjam = namaPeminjam;
+    }
+
+    public LocalDate getTanggalPinjam() {
+        return tanggalPinjam;
+    }
+
+    public void setTanggalPinjam(LocalDate tanggalPinjam) {
+        this.tanggalPinjam = tanggalPinjam;
+    }
+
+    public LocalDate getTanggalKembali() {
+        return tanggalKembali;
+    }
+
+    public void setTanggalKembali(LocalDate tanggalKembali) {
+        this.tanggalKembali = tanggalKembali;
+    }
+
+    public void infoPeminjaman() {
+        System.out.println("=== Informasi Peminjaman ===");
+        System.out.println("Judul Item     : " + item.getJudul());
+        System.out.println("Peminjam       : " + namaPeminjam);
+        System.out.println("Tanggal Pinjam : " + tanggalPinjam);
+        System.out.println("Tanggal Kembali: " + tanggalKembali);
+    }
+}
 
